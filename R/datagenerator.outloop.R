5a4fccc4a9c92a67cfe47e3d4571c218ce05ad3f
Lan Luo <luolan910411@gmail.com>
2023-02-19 17:17:34 -0600

    Update datagenerator.outloop.R

--- R/datagenerator.outloop.R
+++ R/datagenerator.outloop.R
@@ -1,25 +1,24 @@
 datagenerator_out <-
-function(beta, b, n, family, construct, rho, categorical=FALSE, seed=NA){
+function(beta, b, n, family, construct, rho, intercept = TRUE, categorical=FALSE, seed=NA){
   
   if(!is.na(seed)){ set.seed(b+seed) }
   set.seed(b+123456)
   
-  p<-length(beta)
-  if(construct=='ind'){Sigma<- diag(rep(1,p-1))}
-  if(construct=='cs'){Sigma<-matrix(rho,p-1,p-1); diag(Sigma)<-1}
-  if(construct=='ar1'){Sigma<-rho^(abs(outer(1:(p-1), 1:(p-1), "-")))}
-  X <- mvrnorm(n=n, mu=rep(0,p-1), Sigma=Sigma)
+  p <- length(beta)
+  if(construct=='ind'){Sigma<- diag(rep(1, p))}
+  if(construct=='cs'){Sigma<-matrix(rho, p, p); diag(Sigma)<-1}
+  if(construct=='ar1'){Sigma<-rho^(abs(outer(1 : p, 1 : p, "-")))}
+  X <- mvrnorm(n=n, mu=rep(0, p), Sigma=Sigma)
   
   #change half of the X to be categorical variables
-  if(categorical==TRUE){some<-sample(1:(p-1),round((p-1)/2),replace=FALSE); X[,some]<-(X[,some]>0)}
+  if(categorical==TRUE){some<-sample(1 : p,round(p/2),replace=FALSE); X[,some] <- (X[,some]>0)}
   
-  X<-cbind(1,X)
+  if(intercept == TRUE){X[, 1] <- 1}
   
-  if(family=="gaussian"){ y <- X%*%beta + rnorm(n) }
+  if(family=="gaussian"){ y <- X %*% beta + rnorm(n) }
   if(family=="binomial"){ y <- rbinom(n, 1, prob=exp(X%*%beta)/(exp(X%*%beta)+1) ) }
   if(family=="poisson"){y <- rpois(n, exp(X%*%beta)) }
   
-  X<-X[,-1]
-  simdata<-cbind(y,X)
+  simdata <- cbind(y, X)
   return(simdata)
 }
